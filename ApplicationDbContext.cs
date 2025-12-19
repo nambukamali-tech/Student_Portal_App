42387c5126031d1709b7e49e94a515b1c1246b8c
Nambu Kamali <nambukamali@gmail.com>
2025-12-19 15:58:20 +0530

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student_API.Models.Entities;
+namespace Student_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<StudentsInformations> StudentsInformations { get; set; }
+
+    }
+}

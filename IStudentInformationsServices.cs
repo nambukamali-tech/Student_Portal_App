56a5e91aaec00c75d04a3b9457f9a2cb60545456
Nambu Kamali <nambukamali@gmail.com>
2025-12-20 20:02:42 +0530

    Add files via upload

---
+++ IStudentInformationsServices.cs
@@ -0,0 +1,17 @@
+﻿using Students_Portal_App.Models.Entities;
+using Students_Portal_App.ViewModels;
+
+namespace Students_Portal_App.Interfaces
+{
+    public interface IStudentInformationsServices
+    {
+        Task<List<StudentsPortalInfos>> GetStudentsPortalInfosAsync();
+        Task<StudentsPortalInfos> AddStudentsPortalInfosAsync(StudentsPortalInfos studentsPortalInfos);
+        Task UpdateStudentsPortalInfosAsync(StudentsPortalInfos studentsPortalInfos);
+        Task DeleteStudentsPortalInfosAsync(int studentId);
+        Task<StudentsPortalInfos?> GetStudentByIdAsync(int studentId);
+        Task<StudentsIndexViewModel> GetStudentsDashboardAsync();
+        Task UploadStudentPhotoAsync(int studentId, IFormFile photo);
+
+    }
+}

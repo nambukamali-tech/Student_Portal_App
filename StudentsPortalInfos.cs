c818234ba449f387d477c7eb4633b44ef62213b8
Nambu Kamali <nambukamali@gmail.com>
2025-12-20 20:01:18 +0530

    Add files via upload

---
+++ StudentsPortalInfos.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Students_Portal_App.Models.Entities
+{
+    public class StudentsPortalInfos
+    {
+        [Key]
+        public int StudentId { get; set; }
+        [Required]
+        public required string StudentStatus { get; set; }
+        [Required]
+        public required string StudentName { get; set; }
+        public required string Department { get; set; }
+        public DateTime InTime { get; set; }
+        public DateTime OutTime { get; set; }
+        public DateTime LastPresent_New { get; set; }
+        public required string Actions { get; set; }
+        public string? PhotoPath { get; set; }
+
+
+    }
+}

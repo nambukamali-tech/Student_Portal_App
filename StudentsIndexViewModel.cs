76d5c991d764cd451b57a4f9bd887a52b6cda017
Nambu Kamali <nambukamali@gmail.com>
2025-12-21 22:11:20 +0530

    Add files via upload

---
+++ StudentsIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Students_Portal_App.DTOs;
+
+namespace Students_Portal_App.ViewModels
+{
+    //ViewModel only for Index View to show summary and List of students
+    public class StudentsIndexViewModel
+    {
+        public required StudentsSummaryDtos Summary { get; set; }
+        public required List<StudentsListDtos> Students { get; set; }
+        
+    }
+}

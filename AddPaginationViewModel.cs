78e914540b6e8c9ac212527080546f8fd532e9a9
Nambu Kamali <nambukamali@gmail.com>
2025-12-28 10:38:16 +0530

    Add files via upload

---
+++ AddPaginationViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Students_Portal_App.DTOs;
+
+namespace Students_Portal_App.ViewModels
+{
+    
+        public class AddPaginationViewModel
+        {
+            // Data to display (JOIN result)
+            public List<StudentsPaperdtos> Items { get; set; } = new();
+
+            // Pagination info
+            public int CurrentPage { get; set; }
+            public int TotalPages { get; set; }
+            public int PageSize { get; set; }
+
+            // Optional helpers
+            public bool HasPrevious => CurrentPage > 1;
+            public bool HasNext => CurrentPage < TotalPages;
+        }
+    }
+
+

95d61e82857067c030b232cc392915ba8d476ccc
Nambu Kamali <nambukamali@gmail.com>
2025-12-28 10:38:33 +0530

    Add files via upload

---
+++ AddPaperViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+namespace Students_Portal_App.ViewModels
+{
+    public class AddPaperViewModel
+    {
+        public required string RegisterNumber { get; set; }
+
+        public required string PaperTitle { get; set; }
+        public required string PaperDescription { get; set; }
+        public required string PaperCode { get; set; }
+
+    }
+}

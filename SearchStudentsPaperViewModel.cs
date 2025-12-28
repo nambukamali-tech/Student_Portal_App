78156b5a4e8f8b9c1d0627bdc5944322893ff412
Nambu Kamali <nambukamali@gmail.com>
2025-12-28 10:38:52 +0530

    Add files via upload

---
+++ SearchStudentsPaperViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Students_Portal_App.Models.Entities;
+
+namespace Students_Portal_App.ViewModels
+{
+    public class SearchStudentsPaperViewModel
+    {
+          // Search inputs
+            public string? StudentName { get; set; }
+            public string? RegisterNumber { get; set; }
+
+            // Result
+            public StudentsPortalInfos? Student { get; set; }
+            public List<StudentsPaper> StudentsPapers { get; set; } = new();
+        }
+    }
+
+

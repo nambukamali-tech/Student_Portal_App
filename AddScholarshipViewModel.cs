51ef54b091ca62c55ee4382462089f3524b06d6f
Nambu Kamali <nambukamali@gmail.com>
2026-01-02 21:08:35 +0530

    Add files via upload

---
+++ AddScholarshipViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Students_Portal_App.Models.Entities;
+namespace Students_Portal_App.ViewModels
+{
+    public class AddScholarshipViewModel
+    {
+        public required string RegisterNumber { get; set; }
+        public  required string ScholarshipName { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Dob { get; set; }
+        public int ScholarshipYear { get; set; }
+        public string? Country { get; set; }
+        public string? State { get; set; }
+        public string? District { get; set; }
+        public string? Status { get; set; }
+    }
+}

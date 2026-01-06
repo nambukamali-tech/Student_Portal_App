63caed82cc214ab35c8885eb6d55dc2dc93b33eb
Nambu Kamali <nambukamali@gmail.com>
2026-01-06 15:09:32 +0530

    Add files via upload

---
+++ VwStudentsportal.cs
@@ -0,0 +1,18 @@
+﻿namespace Students_Portal_App.Models.Entities
+{
+    public class VwStudentsportal
+    {
+        public int StudentId { get; set; }
+        public string? StudentName { get; set; }
+        public string? RegisterNumber { get; set; }
+        public string? StudentStatus { get; set; }
+        public string? Department { get; set; }
+        public DateTime InTime { get; set; }
+        public DateTime OutTime { get; set; }
+
+        //Studentspapers details
+        public string? PaperTitle { get; set; }
+        public string? PaperCode { get; set; }
+     
+    }
+}

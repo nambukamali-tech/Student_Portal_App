d32814570f846e79cb6a05a7faae321624f46fd5
Nambu Kamali <nambukamali@gmail.com>
2026-01-21 23:03:13 +0530

    Add files via upload

---
+++ IAttendanceService.cs
@@ -0,0 +1,12 @@
+﻿using Students_Portal_App.DTOs;
+using Students_Portal_App.ViewModels;
+
+namespace Students_Portal_App.Interfaces
+{
+    public interface IAttendanceService
+    {
+         Task<IndexViewModel> GetIndexAsync(string statusType);
+
+         Task<AttendanceResultDto> MarkAttendanceAsync(int studentId, DateTime? InTime, DateTime? OutTime);
+    }
+}

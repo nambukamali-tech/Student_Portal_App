3460b6cfc2de7b36bf28a1779baad1d85c9b9960
Nambu Kamali <nambukamali@gmail.com>
2026-01-22 23:08:19 +0530

    Add files via upload

--- IndexViewModel.cs
+++ IndexViewModel.cs
@@ -1,13 +1,19 @@
-﻿namespace Students_Portal_App.ViewModels
+﻿using Students_Portal_App.DTOs;
+
+namespace Students_Portal_App.ViewModels
 {
     public class IndexViewModel
     {
         //For showing total students count
         public int TotalStudents { get; set; }
+        public string StudentStatus { get; set; } = string.Empty;
         //child view models
         public List<StudentRowViewModel> Students { get; set; } = new();
+
+        public List<StudentsAttendanceDto> StudentsAttendance { get; set; } = new();
         public StudentsStatusViewModel StudentsStatus { get; set; } = new();
         public DepartmentDetailsViewModel DepartmentDetails { get; set; } = new();
+        public AttendanceResultDto AttendanceSummary { get; set; } = new();
 
     }
     //For showing summary of students in index page students table
@@ -20,7 +26,6 @@
         public string DepartmentName { get; set; } = string.Empty;
         public DateTime? InTime { get; set; }
         public DateTime? OutTime { get; set; }
-
         public string CurrentStatus { get; set; } = string.Empty;
         public string MonthlyStatus { get; set; } = string.Empty;
         

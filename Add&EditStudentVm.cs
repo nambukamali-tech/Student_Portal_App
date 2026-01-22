081bc76c585b209eb6945b16dee3f0f6bcb7baa6
Nambu Kamali <nambukamali@gmail.com>
2026-01-22 23:08:01 +0530

    Add files via upload

---
+++ Add&EditStudentVm.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Students_Portal_App.ViewModels
+{
+    public class Add_EditStudentVm
+    {
+        public int StudentId { get; set; }
+        [Required]
+        public string? RegisterNumber { get; set; }
+        [Required]
+        public string? StudentName { get; set; }
+        [Required]
+        public int? DepartmentId { get; set; }
+        public string? PhotoPath { get; set; }
+    }
+}

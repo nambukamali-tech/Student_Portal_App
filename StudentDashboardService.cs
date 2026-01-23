1e2e2854818e86c2964173f67d5d45b301988d89
Nambu Kamali <nambukamali@gmail.com>
2026-01-23 23:12:29 +0530

    Add files via upload

---
+++ StudentDashboardService.cs
@@ -0,0 +1,34 @@
+﻿using Students_Portal_App.Data;
+using Students_Portal_App.ViewModels;
+using Students_Portal_App.Interfaces;
+
+namespace Students_Portal_App.Services
+{
+    public class StudentDashboardService : IDashboardService
+    {
+        private readonly StudentsService _studentsService;
+
+        public StudentDashboardService(StudentsService studentsService)
+        {
+            _studentsService = studentsService;
+        }
+        public async Task<StudentDashboardVm?> GetStudentDashboardAsync(string regno)
+        {
+            if (string.IsNullOrEmpty(regno))
+                return null;
+
+            var dashboardData = await _studentsService.GetStudentDashboardAsync(regno);
+            if (dashboardData == null)
+                return null;
+
+            // Calculate Present & Absent for showing the dashboard page
+            dashboardData.PresentCount = dashboardData.MonthlyAttendance
+                .Count(a => a.InTime != null);
+
+            dashboardData.AbsentCount = dashboardData.MonthlyAttendance
+                .Count(a => a.InTime == null);
+
+            return dashboardData;
+        }
+    }
+}

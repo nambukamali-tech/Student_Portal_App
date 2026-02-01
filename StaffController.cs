8062075516cc645dfbf6fc9f6b1c005d45234b15
Nambu Kamali <nambukamali@gmail.com>
2026-02-01 21:22:05 +0530

    Add files via upload

---
+++ StaffController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Students_Portal_App.Interfaces;
+using Students_Portal_App.ViewModels;
+
+namespace Students_Portal_App.Controllers
+{
+    public class StaffController : Controller
+    {
+        private readonly IStudentServiceInterface _studentService;
+        public StaffController(IStudentServiceInterface studentService)
+        {
+            _studentService = studentService;
+        }
+        public async Task<IActionResult> StaffDashboard()
+        {
+            StaffDashboardViewModel model = await _studentService.GetStaffDashboardAsync();
+            return View(model);
+        }
+
+    }
+}
